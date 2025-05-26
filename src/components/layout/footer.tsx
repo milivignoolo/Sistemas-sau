0090aac2128a11f8a69063be5c1d9d050056720d
Milagros Vignolo <milagrosvignolo05@gmail.com>
2025-05-26 11:22:22 -0300

    Delete src directory

--- src/components/layout/footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-secondary text-secondary-foreground py-6 mt-12">
-      <div className="container mx-auto px-4 text-center text-sm">
-        © {new Date().getFullYear()} Secretaría de Asuntos Universitarios (SAU) - UTN. Todos los derechos reservados.
-      </div>
-    </footer>
-  );
-}
